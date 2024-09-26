5b709b7b15b096170df98655c191fbb4e0b2ce15
AresNeutron <arexneutron@gmail.com>
2024-09-26 15:27:28 -0300

    Uploading project to Github

---
+++ src/Helpers/Questions.js
@@ -0,0 +1,26 @@
+export const Questions = [
+    {
+        prompt: 'What is 1+3?',
+        optionA: '24',
+        optionB: '21',
+        optionC: '4',
+        optionD: '2',
+        answer: 'C'
+    },
+    {
+        prompt: 'What is the capital of France?',
+        optionA: 'Berlin',
+        optionB: 'Madrid',
+        optionC: 'Paris',
+        optionD: 'Rome',
+        answer: 'C'
+    },
+    {
+        prompt: 'Which planet is known as the Red Planet?',
+        optionA: 'Earth',
+        optionB: 'Mars',
+        optionC: 'Jupiter',
+        optionD: 'Saturn',
+        answer: 'B'
+    }
+];
